a1574d06784496754bfb00dd1c4128f671449128
Anastasiia Paliha <nastya.paliha@gmail.com>
2024-12-09 23:44:28 +0200

    make build

---
+++ index.b1593478.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("thead"),o=document.querySelector("tfoot"),r=document.querySelector("tbody");function t(e){Array.from(e.rows).forEach(function(e){var o=e.cells[1].cloneNode(!0);e.insertBefore(o,e.lastElementChild)})}t(e),t(o),t(r);
+//# sourceMappingURL=index.b1593478.js.map
